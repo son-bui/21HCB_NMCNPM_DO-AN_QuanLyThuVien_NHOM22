6963c41b7759e42d716353f87480485c319659ee
Phạm Quang Trung <mrtrung081099@gmail.com>
2022-03-05 16:37:24 +0700

    fix miss code

--- QuanLyThuVien/Entities/Models/NhanVien.cs
+++ QuanLyThuVien/Entities/Models/NhanVien.cs
@@ -1,8 +1,21 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
-namespace Entities
+namespace Entities.Models
 {
-    public class Class1
+    public class NhanVien
     {
+        [Column("NhanVienId")]
+        [Required(ErrorMessage = "Tên nhân viên không được bỏ trống.")]
+        [MaxLength(60, ErrorMessage = "Tối đa 60 kí tự.")]
+        public Guid Id { get; set; }
+        public string HoTen { get; set; }
+        public string DiaChi { get; set; }
+        public DateTime NgaySinh { get; set; }
+        public string SDT { get; set; }
+        public string BangCap { get; set; }
+        public string BoPhan { get; set; }
+        public string ChucVu { get; set; }
     }
 }
